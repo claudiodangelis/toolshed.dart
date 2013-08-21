1af710e549aeddf3a138f26895bfb596247beb96
Damon Douglas <douglas.damon@gmail.com>
2013-08-21 16:54:45 -0400

    revert run.sh changes

--- drone/run.sh
+++ drone/run.sh
@@ -1,29 +1,2 @@
 #!/bin/bash
 dart tool/hop_runner.dart analyze_hop
-rm -rf test
-mkdir test
-cd test
-mkdir cla
-cd cla
-dart ../../tool/hop_runner.dart cla cla
-pub install
-dart tool/hop_runner.dart analyze_libs
-cd ..
-mkdir cpa
-cd cpa
-dart ../../tool/hop_runner.dart cpa cpa
-pub install
-dart tool/hop_runner.dart analyze_libs
-cd ..
-mkdir pa
-cd pa
-dart ../../tool/hop_runner.dart pa pa
-pub install
-dart tool/hop_runner.dart analyze_libs
-cd ..
-mkdir wa
-cd wa
-dart ../../tool/hop_runner.dart wa wa
-pub install
-dart tool/hop_runner.dart analyze_libs
-
