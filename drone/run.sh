5cbe74d2237d0b44aa86442c91fbb1b3a6ea0191
Damon Douglas <douglas.damon@gmail.com>
2013-09-02 18:42:18 -0400

    update drone to build test chrome app and analyze_libs

--- drone/run.sh
+++ drone/run.sh
@@ -3,8 +3,16 @@ dart tool/hop_runner.dart analyze_hop
 rm -rf test
 mkdir test
 cd test
+
 mkdir cla
 cd cla
 dart ../../tool/hop_runner.dart cla cla
+pub install
+dart tool/hop_runner.dart analyze_libs
+
+cd ..
+mkdir cpa
+cd cpa
+dart ../../tool/hop_runner.dart cpa cpa
 pub install
 dart tool/hop_runner.dart analyze_libs
